b1b478bb27e52d25930d50770567222967e73a29
Gowrisankarmahesh <Gowri@#>
2026-06-19 16:01:43 +0530

    first commit

---
+++ src/Task1.ts
@@ -0,0 +1,36 @@
+//question 1
+let employeeName:string = "Mahesh";
+let employeeId :number = 50;
+let department: string = "IT";
+let salary : number = 20000;
+let experience : number =6;
+let location :string="chennai";
+//question 2
+ if(salary>=20000){
+    console.log("Eligible for bonus");
+ }
+    else
+        {
+        console.log("Not eligible for bonus");
+    }
+ 
+ //question 3
+ 
+ if(experience>=5){
+    console.log("employee is senior");
+ }
+ else{
+        console.log("employee is junior");
+    }
+ 
+
+//question 4
+if(location==="Chennai"){
+    console.log("candidate work location is chennai");
+}
+else{
+    console.log("candidate work location is not chennai");
+    }
+//question 5
+let Annualsalary:number=salary*12;
+
