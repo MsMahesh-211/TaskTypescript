db57a943eb502e2354250f11d33fb4dc7abfec37
Gowrisankarmahesh <Gowri@#>
2026-06-22 14:03:25 +0530

    add

---
+++ src/controlflowstask.ts
@@ -0,0 +1,58 @@
+//Question 1
+let score :number =75;
+if(score >=50){
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
+//Question 2
+let temp : number=35;
+if(temp>=30){
+    console.log("Hot");
+
+}
+else{
+    console.log("Normal");
+}
+//Question 3
+let browser:String="chrome";
+switch(browser){
+    case "chrome":
+        console.log("chrome browser");
+        break;
+        default:
+            console.log("other browser");
+}
+//question 4
+let i:number=1;
+for(i=1;i<=20;i++)
+    console.log(i);
+//question 5
+let sum = 1+2+3+4+5+6+7+8+9+10;
+console.log(sum);
+//question 6
+let sum=0;
+let i=1;
+for(i=1;i<=10;i++)
+    sum+=i;
+console.log(sum);
+//question 7
+let i=1;
+for(i=1;i<=10;i++){
+   console.log(5*i);
+}
+//question 8
+let i=2;
+while(i<=20){
+    console.log(i);
+    i+=2;
+}
+//question 9
+let i=5;
+do{
+    console.log(i);
+    i--;
+}while(i>=1);
+
+    
